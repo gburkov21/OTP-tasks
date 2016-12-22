7e2535e4556abab247723d5f82f400a1ebeb3241
Goshan <Goshan>
2016-12-22 11:39:35 +0300

    edit task PhoneNumber

--- PhoneBook/src/main/java/ru/gburkov/SearchPhoneNumbers.java
+++ PhoneBook/src/main/java/ru/gburkov/SearchPhoneNumbers.java
@@ -3,19 +3,16 @@ package ru.gburkov;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 
 class SearchPhoneNumbers {
-    private final HashMap<String, ArrayList> map = new HashMap<>();
+    private final HashMap<String, ArrayList<String>> map = new HashMap<>();
 
     SearchPhoneNumbers() {
-        ArrayList<String> ivanovNumbers = new ArrayList<>(Arrays.asList("+8 800 2000 500", "+8 800 2000 600"));
-        ArrayList<String> petrovNumbers = new ArrayList<>(Arrays.asList("+8 800 2000 700"));
-        ArrayList<String> sidorovNumbers = new ArrayList<>(Arrays.asList("+8 800 2000 800", "+8 800 2000 900", "+8 800 2000 000"));
-
-        map.put("Иванов И.И.", ivanovNumbers);
-        map.put("Петров П.П.", petrovNumbers);
-        map.put("Сидоров С.С.", sidorovNumbers);
+        map.put("Иванов И.И.", new ArrayList<>(Arrays.asList("+8 800 2000 500", "+8 800 2000 600")));
+        map.put("Петров П.П.", new ArrayList<>(Arrays.asList("+8 800 2000 700")));
+        map.put("Сидоров С.С.", new ArrayList<>(Arrays.asList("+8 800 2000 800", "+8 800 2000 900", "+8 800 2000 000")));
     }
 
     void phoneSearch(String value) {
